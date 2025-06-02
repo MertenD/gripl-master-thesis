be6b2ddbf44d59aeab3d137fe63be52baeac9297
MertenD <merten.dieckmann@web.de>
2025-06-02 19:57:48 +0200

    Use environment variable for api url

--- gripl/gripl-frontend/next.config.ts
+++ gripl/gripl-frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:8080/:path*',
+                destination: `${process.env.API_BASE_URL}/:path*`,
             },
         ];
     },
