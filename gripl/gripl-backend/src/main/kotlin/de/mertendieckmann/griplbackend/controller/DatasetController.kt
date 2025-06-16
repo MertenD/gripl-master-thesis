e318d4d830409f3bc0df2cdb16c52e20b0398181
MertenD <merten.dieckmann@web.de>
2025-06-16 22:51:26 +0200

    Implement dataset preview generator in backend

--- gripl/gripl-backend/src/main/kotlin/de/mertendieckmann/griplbackend/controller/DatasetController.kt
+++ gripl/gripl-backend/src/main/kotlin/de/mertendieckmann/griplbackend/controller/DatasetController.kt
@@ -1,10 +1,11 @@
 package de.mertendieckmann.griplbackend.controller
 
+import BpmnConverter
 import de.mertendieckmann.griplbackend.model.dto.EvaluationData
 import de.mertendieckmann.griplbackend.repository.EvaluationDataRepository
-import org.camunda.bpm.model.bpmn.Bpmn
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,11 +21,27 @@ class DatasetController(
     @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getBpmnDataset(@PathVariable("id") id: Long): EvaluationData {
         val datasetEntry = evaluationDataRepository.getEvaluationDataById(id)
-
-        if (datasetEntry == null) {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "No dataset entry found for Id: $id")
-        }
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "No dataset entry found for Id: $id")
 
         return datasetEntry
+    }
+
+    @GetMapping("/{id}/preview", produces = [MediaType.TEXT_XML_VALUE])
+    fun getSvg(@PathVariable id: Long): ResponseEntity<String> {
+        val bpmnConverter = BpmnConverter()
+
+        val datasetEntry = evaluationDataRepository.getEvaluationDataById(id)
+            ?: return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Kein Datensatz gefunden für Id: $id")
+        val bpmnXml = datasetEntry.bpmnXml
+
+        val svg = try {
+            bpmnConverter.convertXmlToSvg(bpmnXml)
+        } catch (ex: IllegalArgumentException) {
+            return ResponseEntity.badRequest().body("Fehler beim Parsen: ${ex.message}")
+        } catch (ex: Exception) {
+            return ResponseEntity.status(500).body("Serverfehler: ${ex.message}")
+        }
+
+        return ResponseEntity.ok(svg)
     }
 }
