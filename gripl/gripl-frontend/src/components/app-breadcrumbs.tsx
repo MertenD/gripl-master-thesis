90d12c57d992fe06a09ea893c9a7ebe665ebd4d7
MertenD <merten.dieckmann@web.de>
2025-06-30 22:14:57 +0200

    Display test case name instead of id in breadcrumbs

--- gripl/gripl-frontend/src/components/app-breadcrumbs.tsx
+++ gripl/gripl-frontend/src/components/app-breadcrumbs.tsx
@@ -4,15 +4,52 @@ import {usePathname} from "next/navigation";
 import {generateBreadcrumbs} from "@/lib/breadcrumbs";
 import {Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbSeparator} from "@/components/ui/breadcrumb";
 import Link from "next/link";
+import {useEffect, useState} from "react";
+import {Crumb} from "@/models/Crumb";
+import {EvaluationData} from "@/models/dto/EvaluationData";
+
+const nameMap = {
+    "": 'Sandbox',
+    labeling: "Labeling",
+    thesis: "Thesis",
+}
 
 export default function AppBreadCrumbs() {
 
     const pathname = usePathname()
-    const crumbs = generateBreadcrumbs(pathname, {
-        "": 'Sandbox'
-    })
+    const [crumbs, setCrumbs] = useState<Crumb[]>(generateBreadcrumbs(pathname, nameMap))
 
-    console.log("Breadcrumbs:", crumbs)
+    useEffect(() => {
+
+        const newCrumbs = generateBreadcrumbs(pathname, nameMap);
+
+        // If the user is on the labeling page and has a test case selected the name of the test case should be displayed instead of the id
+        if (newCrumbs[0].label === nameMap["labeling"] && newCrumbs.length === 2) {
+            fetch(`/api/dataset/${newCrumbs[1].label}`).then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch test case with id ${newCrumbs[1].label}: ${response.statusText}`);
+                }
+                return response.json()
+            }).then((data: EvaluationData) => {
+                const updatedCrumbs = newCrumbs.map((crumb, index) => {
+                    if (index === 1) {
+                        return {
+                            ...crumb,
+                            label: `${data.name} (${data.id})`,
+                        };
+                    }
+                    return crumb;
+                });
+                setCrumbs(updatedCrumbs);
+            }).catch(error => {
+                console.error("Error fetching dataset name:", error);
+                setCrumbs(newCrumbs)
+            })
+            return
+        }
+
+        setCrumbs(newCrumbs)
+    }, [pathname])
 
     return <Breadcrumb>
         <BreadcrumbList>
