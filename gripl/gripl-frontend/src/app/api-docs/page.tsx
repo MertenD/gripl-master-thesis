c4e0f91226611b02eaa1d83effc39ab703925a20
MertenD <merten.dieckmann@web.de>
2025-07-07 22:51:33 +0200

    Render api docs page dynamic at runtime

--- gripl/gripl-frontend/src/app/api-docs/page.tsx
+++ gripl/gripl-frontend/src/app/api-docs/page.tsx
@@ -5,6 +5,8 @@ import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 import "@/style/swagger/swagger-overrides.css"
 
+export const dynamic = 'force-dynamic';
+
 export default function ApiDocsPage() {
 
     return <SwaggerUI url="/api/v3/api-docs.yaml" />
