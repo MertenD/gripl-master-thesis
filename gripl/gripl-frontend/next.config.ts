585416bb178adb34bda5e0db008d172d62740871
MertenD <merten.dieckmann@web.de>
2025-07-07 22:47:47 +0200

    Disable strict mode for swagger

--- gripl/gripl-frontend/next.config.ts
+++ gripl/gripl-frontend/next.config.ts
@@ -16,7 +16,8 @@ const nextConfig: NextConfig = {
                 destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
             }
         ];
-    }
+    },
+    reactStrictMode: false
 };
 
 export default nextConfig;
